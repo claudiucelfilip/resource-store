669aa4faec68d62214399ec8d1a9f6eca9b0e608
Claudiu Filip <claudiu.filip@ness.com>
2018-12-08 13:20:18 +0200

    Action based (#3)
    * - implements dispatch action functionality and adds property setters
    
    * - fixes npm audit problems
    
    * fixes parent refferences
    
    * - removes faulty type constraint

--- src/resourceStore.ts
+++ src/resourceStore.ts
@@ -1,11 +1,11 @@
-import { symbol } from './utils';
-import { Resource } from './resource';
-import { IResource } from '.';
+import { symbol } from "./utils";
+import { Resource } from "./resource";
+import { IResource } from ".";
 
 export class ResourceStore {
   private stores = {};
 
-  constructor() { }
+  constructor() {}
 
   add(resource: Resource): IResource {
     const store: IResource = this.makeStore(resource);
@@ -28,25 +28,27 @@ export class ResourceStore {
   makeStore(resource: Resource) {
     let store = new Proxy(resource, {
       get: (target: Resource, name: string) => {
-
-        if (name === 'dispatch') {
+        if (name === "dispatch") {
           return target[name].bind(this.get(target));
         }
 
         if (target[name] !== undefined) {
-          if (typeof target[name] === 'function') {
+          if (typeof target[name] === "function") {
             return target[name].bind(target);
           }
           return target[name];
         }
 
-        if (typeof name === 'symbol') {
+        if (typeof name === "symbol") {
           return target[name];
         }
 
-        name = name.replace(/\$$/, '');
-        const compoundKey = (`${target[symbol.key]}.${name}`);
-        return this.get(compoundKey, true) || this.makeStore(resource[symbol.select](name)); //this.get(resource[symbol.select](name));
+        name = name.replace(/\$$/, "");
+        const compoundKey = `${target[symbol.key]}.${name}`;
+        return (
+          this.get(compoundKey, true) ||
+          this.makeStore(resource[symbol.select](name))
+        ); //this.get(resource[symbol.select](name));
       }
     });
 
@@ -54,12 +56,14 @@ export class ResourceStore {
     return store;
   }
 
-  get(keyResource: string | Resource, dontThrow = false): IResource {
+  get(keyResource: string | Resource, dontThrow = false) {
     let key: string = keyResource[symbol.key] || keyResource;
     let resource = this.stores[key];
 
     if (!resource && !dontThrow) {
-      throw new Error(`No resource with key ${key[symbol.key] || key} was defined.`);
+      throw new Error(
+        `No resource with key ${key[symbol.key] || key} was defined.`
+      );
     }
 
     return resource;
